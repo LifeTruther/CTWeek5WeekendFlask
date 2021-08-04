ba3be552c5385eddff39763f9dbd5c2cf74de98e
LifeTruther <walkerian7@gmail.com>
2021-08-03 21:33:10 -0500

    day 2

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
+import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 # this bit of code sets the path for flask
+
+#SECRET KEY
+class Config:
+
+    """
+        Sets configuration variables for our Flask app here.
+        Eventually will use hidden variable items - but for now,
+        we'll leave them exposed in config
+    """
+    SECRET_KEY = "You will guess..."
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
